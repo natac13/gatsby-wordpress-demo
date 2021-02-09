871f49901571a5ec0e977207bc3737fde20ade03
Sean Campbell <sean.campbell13@gmail.com>
2021-02-09 13:11:31 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: "gatsby-wordpress",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        url: "http://bedrock.local/graphql",
+      },
+    },
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
